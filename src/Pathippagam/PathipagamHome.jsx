8defe54af0b064b76a0909d9b7d619e34ec51e43
Thamilarasan <thamilarasan.gp2023it@sece.ac.in>
2025-03-13 11:49:15 +0530

    gallerupadated

--- src/Pathippagam/PathipagamHome.jsx
+++ src/Pathippagam/PathipagamHome.jsx
@@ -4,14 +4,18 @@ import PathipagamEvent from './PathipagamEvent'
 import NavBar from '../User/NavBar'
 import ContactForm from '../User/ContactForm'
 import Footer from '../User/Footer'
+import { LanguageProvider } from "../LanguageContext"; 
 const PathipagamHome = () => {
   return (
     <div>
+          <LanguageProvider>
         <NavBar/>
         <PathipagamBooks/>
         <PathipagamEvent/>
         <ContactForm/>
         <Footer/>
+          </LanguageProvider>
+       
     </div>
   )
 }
